3c53e87fa2fd9939cc2c549a8ea5db9ea9d008a8
Yura Lazarev <Unisay@users.noreply.github.com>
2024-03-21 20:27:14 +0100

    no bitwise or trick for ints

--- src/Data/Semiring.lua
+++ src/Data/Semiring.lua
@@ -1,6 +1,6 @@
 return {
-  intAdd = function(x) return function(y) return x + y | 0 end end,
-  intMul = function(x) return function(y) return x * y | 0 end end,
+  intAdd = function(x) return function(y) return x + y end end,
+  intMul = function(x) return function(y) return x * y end end,
   numAdd = function(x) return function(y) return x + y end end,
   numMul = function(x) return function(y) return x * y end end
 }
